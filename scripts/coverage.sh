0d67cd56d093d1b0d19150e821c57339246195e0
Yehiyam Livneh <yehiyam@gmail.com>
2021-01-11 11:09:48 +0200

    coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -3,5 +3,8 @@
 set -ex
 
 pip install coveralls
+pip install -r tests/requirements.txt
+python setup.py bdist_wheel
+pip install $PWD/dist/*
 coverage run --source=hkube_debbuging_python_api -m pytest tests/
 coveralls
